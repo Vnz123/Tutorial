b6006ec79dde2e20ebb73c1a0b25be7b76cace0b
VANZIC\vanzi <vanzicbarreto123gmail.com>
2024-07-05 11:47:58 +0530

    final changes

--- test2.js
+++ test2.js
@@ -1,16 +1,2 @@
 
-const http= ('http');
-//import express from express;
-
-
-
-//const app= express();
-
-const port=5000;
-
-
-http.get('/',(req,res)=> {
-     console.log('file working');
-    res.send('Hello from homepage.');
-});
-http.listen(port,()=> console.log(`Server running on port: http://localhost:${port }`));
+console.log('this is file2');
